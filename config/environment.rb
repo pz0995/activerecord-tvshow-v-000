0b5aed2ecb33f24ecfa134009b689820391e155d
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-09-17 05:43:06 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -12,7 +12,7 @@ Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| requir
 Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each {|f| require f}
 
 connection_details = YAML::load(File.open('config/database.yml'))
-
+#
 # DBRegistry[ENV["ACTIVE_RECORD_ENV"]].connect!
 DB = ActiveRecord::Base.establish_connection(connection_details)
 
